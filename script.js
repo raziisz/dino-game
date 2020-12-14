fab66b9d51bfd583d763031fadc94c9354f7057c
Luiz Felipe <raziel_libertino@hotmail.com>
2020-12-14 11:44:04 -0400

    colision

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
   if(event.keyCode === 32 && !isJumping) {
@@ -10,7 +11,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-  let position = 0;
   isJumping = true;
   let upInterval = setInterval(() => {
     if (position >= 150) {
@@ -47,6 +47,10 @@ function createCactus() {
     if (cactusPosition < -60) {
       clearInterval(leftInterval);
       background.removeChild(cactus);
+    } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+      // Game over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>'
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
